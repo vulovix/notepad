404365d4feace67e81167f75c7a1d254aed7aa80
IvanVulovic <ivn.vulovic@gmail.com>
2024-02-10 02:59:40 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Notepad
+
+This tool lets you create text notes.
+
+### Resources
+
+[favicon.png](https://www.flaticon.com/free-icon/notepad_5066321)
---
+++ script.js
@@ -0,0 +1,46 @@
+const defaultValue = `- made with notepad.iksos.com
+ 
+`;
+window.addEventListener("load", () => {
+  const editor = document.querySelector("#editor");
+  const storage = localStorage.getItem("xOS_Notepad");
+  if (storage) {
+    editor.value = storage;
+  } else if (storage === null) {
+    editor.value = defaultValue;
+  }
+  editor.focus();
+});
+window.addEventListener("beforeunload", () => {
+  const currentText = getTextToCopy();
+  saveToStorage(currentText);
+});
+function copyToClipboard(text) {
+  window.navigator.clipboard
+    .writeText(text)
+    .then(() => console.log("Copied to clipboard!"))
+    .catch(() => null);
+}
+function saveToStorage(text) {
+  localStorage.setItem("xOS_Notepad", text);
+}
+function getTextToCopy() {
+  const textToCopy = document.querySelector("#editor");
+  return textToCopy.value;
+}
+
+document.addEventListener(
+  "keydown",
+  function (e) {
+    if (
+      e.key === "s" &&
+      (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)
+    ) {
+      e.preventDefault();
+      const currentText = getTextToCopy();
+      saveToStorage(currentText);
+      copyToClipboard(currentText);
+    }
+  },
+  false
+);
